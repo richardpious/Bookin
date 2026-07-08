e9fd6cbcf577da6d548a82fe12c8b7f7ba8ab145
Richard Xavier Pious <richardpious369@gmail.com>
2026-07-08 17:34:26 +0530

    bug fix

--- platform/frontend/bookin/vite.config.js
+++ platform/frontend/bookin/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/static/',
   server: {
     proxy: {
       '/update-file': 'http://127.0.0.1:8000',
@@ -13,3 +14,4 @@ export default defineConfig({
     }
   }
 })
+
