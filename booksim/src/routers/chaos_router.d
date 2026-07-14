04a18afab1e0d6746fafefacaf0b3c52f7db287b
Richard Xavier Pious <richardpious369@gmail.com>
2026-07-14 14:35:58 +0530

    gitignored some extra booksim compiled files inside src/<folders>

--- booksim/src/routers/chaos_router.d
+++
@@ -1,8 +0,0 @@
-routers/chaos_router.o: routers/chaos_router.cpp booksim.hpp \
- routers/chaos_router.hpp module.hpp booksim.hpp routers/router.hpp \
- timed_module.hpp module.hpp flit.hpp outputset.hpp credit.hpp \
- flitchannel.hpp channel.hpp globals.hpp timed_module.hpp flit.hpp \
- channel.hpp config_utils.hpp allocators/allocator.hpp routefunc.hpp \
- routers/router.hpp config_utils.hpp outputset.hpp buffer_state.hpp \
- credit.hpp pipefifo.hpp vc.hpp routefunc.hpp random_utils.hpp \
- globals.hpp
